c7fa321ede0a328aaf6925478e402523c6fb12a6
Anker1818 <angelgabrielquinteromorales@gmail.com>
2024-11-10 00:26:48 -0500

    s

--- Base.js
+++ Base.js
@@ -4,6 +4,8 @@ import { VRButton } from 'three/addons/webxr/VRButton.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+scene.background = new THREE.Color(0xccfffb);
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
@@ -11,17 +13,34 @@ document.body.appendChild( renderer.domElement );
 renderer.xr.enabled = true;
 document.body.appendChild( VRButton.createButton( renderer ) );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
+//Texturas
+const texturePasto = new THREE.TextureLoader().load('Assets/Pasto1.jpg'); 
+const textureArbol = new THREE.TextureLoader().load('Assets/Arbol1.jpg'); 
+
+
+// Materiales
+const materialPasto1 = new THREE.MeshBasicMaterial( { map:texturePasto } );
+const materialArbol1 = new THREE.MeshBasicMaterial( { map:textureArbol } );
+
+
+
+const geometry = new THREE.BoxGeometry( 30, 1, 30 );
+const cube = new THREE.Mesh( geometry, materialPasto1 );
+cube.position.set(0, -1, 0);
 scene.add( cube );
+
+
+const geometry1 = new THREE.BoxGeometry( 2, 10, 2 );
+const arbol = new THREE.Mesh( geometry1, materialArbol1 );
+arbol.position.set(5, 5, 5);
+scene.add( arbol );
+
 
 camera.position.z = 5;
 
 function animate() {
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+
 
 	renderer.render( scene, camera );
 
