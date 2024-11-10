a6127b1df885f4920f7bded0f283d0c367aae8fe
Anker1818 <angelgabrielquinteromorales@gmail.com>
2024-11-10 00:13:07 -0500

    n

--- Base.js
+++ Base.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { VRButton } from 'three/addons/webxr/VRButton.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -7,6 +8,8 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
+renderer.xr.enabled = true;
+document.body.appendChild( VRButton.createButton( renderer ) );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
